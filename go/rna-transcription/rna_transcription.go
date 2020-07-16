db51a717738ac8c5d32442d21826db89c59fd767
Paweł Dworniczak <dworniczakpawel@gmail.com>
2020-07-17 00:45:04 +0200

    add rna solution

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,4 +1,21 @@
 package strand
 
+// ToRNA function transform dna code to rna
 func ToRNA(dna string) string {
+	result := make([]rune, len(dna))
+
+	for i, char := range dna {
+		switch char {
+		case 'G':
+			result[i] = 'C'
+		case 'C':
+			result[i] = 'G'
+		case 'T':
+			result[i] = 'A'
+		case 'A':
+			result[i] = 'U'
+		}
+	}
+
+	return string(result)
 }
